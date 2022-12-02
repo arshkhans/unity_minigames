61f3185b6014fa1b1d1d351fd3fda9f54ab01922
Arsh Khan <khanarsh322@gmail.com>
2022-12-02 22:14:33 +0530

    Removed a single line of print

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -73,8 +73,9 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
-        if (flatVel.magnitude > moveSpeed){
-            Debug.Log("slow");
+        // limit velocity if needed
+        if(flatVel.magnitude > moveSpeed)
+        {
             Vector3 limitedVel = flatVel.normalized * moveSpeed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
